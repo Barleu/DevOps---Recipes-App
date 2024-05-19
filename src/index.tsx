a0d7bc75171bb23d5f53d8e4535102b843cdf75a
Barleu <75945074+Barleu@users.noreply.github.com>
2024-05-20 01:20:09 +0300

    Add test feature flag (#2)

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import FlagProvider from "@unleash/proxy-client-react";
+
+const config = {
+  url: "http://localhost:4242/api/frontend", // Your front-end API URL or the Unleash proxy's URL (https://<proxy-url>/proxy)
+  clientKey:
+    "*:development.fff6eee80f717f8aac748fdb5d4854f071ab499e9d6274dcc287ff15", // A client-side API token OR one of your proxy's designated client keys (previously known as proxy secrets)
+  refreshInterval: 15, // How often (in seconds) the client should poll the proxy for updates
+  appName: "recipes-app", // The name of your application. It's only used for identifying your application
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <FlagProvider config={config}>
+      <App />
+    </FlagProvider>
   </React.StrictMode>
 );
 
